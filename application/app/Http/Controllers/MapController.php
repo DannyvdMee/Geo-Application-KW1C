bae2d9d0f3897db182f5d2df88c240e65ecc6bfb
Danny van der Mee <danny.vandermee@edu-kw1c.nl>
2018-06-20 10:47:53 +0200

    The recieving of the POI's are placed in a seperate function

--- application/app/Http/Controllers/MapController.php
+++ application/app/Http/Controllers/MapController.php
@@ -6,14 +6,19 @@ use Illuminate\Http\Request;
 
 class MapController extends Controller
 {
-    public function index()
-    {
-    	$pois = [
+	public function index()
+	{
+		return view('map/index', ['pois' => $this->recievePOI()]);
+	}
+
+	public function recievePOI()
+	{
+		$pois = [
 			['avans', 51.6886659, 5.2869727, 'Description of Avans University POI'],
 			['kw1c', 51.6904646, 5.2867472, 'Description of KW1C POI'],
 			['cs', 51.689968, 5.295078, 'Description of Central Station POI'],
 		];
 
-        return view('map/index', ['pois' => $pois]);
-    }
+		return $pois;
+	}
 }
