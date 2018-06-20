8bff775224bee863275ab6af8054a69d09d36193
Danny van der Mee <danny.vandermee@edu-kw1c.nl>
2018-06-20 12:03:55 +0200

    Created a department modal and updated the department controller

---
+++ application/app/Department.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Danny
+ * Date: 20-Jun-18
+ * Time: 12:01
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+
+}
--- application/app/Http/Controllers/Admin/DepartmentController.php
+++ application/app/Http/Controllers/Admin/DepartmentController.php
@@ -7,5 +7,29 @@ use App\Http\Controllers\Controller;
 
 class DepartmentController extends Controller
 {
-    //
+	public function approveDepartment($department){
+
+		// Here a SQL search is done to search for the department and set it it's active state on true
+
+		$resultCreatePrefix = $this->creatingPrefix($department);
+
+	}
+
+	public function overviewDepartment(){
+		$avalibleDepartments = [
+			'Kappersopleiding' => true,
+			'ICT-Academie' => false,
+			'TheaterOpleiding' => false
+		];
+
+		return $avalibleDepartments;
+	}
+
+	public function editApprovementDepartment(){
+
+	}
+
+	public function creatingPrefix($department){
+
+	}
 }
