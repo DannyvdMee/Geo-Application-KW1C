2b022a4e50a0141371f2743eb446e675fe7a4a05
Danny van der Mee <danny.vandermee@edu-kw1c.nl>
2018-06-22 20:10:06 +0200

    Created the teacher factory, but we must wait for a migration

---
+++ application/app/Factories/TeacherFactory.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Danny
+ * Date: 22-Jun-18
+ * Time: 20:08
+ */
+
+namespace App\User\Factory;
+
+
+class TeacherFactory
+{
+	// Waiting for a database teacher migration be like:
+	// Fuck this takes too long Ibo...
+}
