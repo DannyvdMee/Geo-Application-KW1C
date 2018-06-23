1917d631cf036228ed7c890403d09a6f578d51dd
Rinaldo Boeje <rinaldo.boeje@edu-kw1c.nl>
2018-06-23 18:48:51 +0200

    Index group added

--- application/resources/views/teacher/group/index.blade.php
+++ application/resources/views/teacher/group/index.blade.php
@@ -3,25 +3,46 @@
 @section('content')
 	<div class="row">
 		<div class="col">
-			<div class="row">
-				<div class="col">
-					<h1 class="text-center">@lang('messages.students')</h1>
-				</div>
-			</div>
-			<div class="row" id="poi-container">
-				<div class="col">
+			<section>
+				<h2 class="text-center">@lang('messages.groups')</h2>
+
+				<div id="department-container">
 					@foreach($groups as $group)
 						<div class="row">
-							<div class="col">
-								<p>{{ $group->name }}</p>
+							<div class="col dataItem">
+								<p class="display-inline-block">{{ $group->title }}</p>
+								<div class="float-right">
+									@if ($group->state == true)
+										<a href="">{{--TODO: Href pages when files are made--}}
+											<i class="material-icons">visibility</i>
+										</a>
+									@else
+										<a href="">{{--TODO: Href pages when files are made--}}
+											<i class="material-icons">visibility_off</i>
+										</a>
+									@endif
+									<a href="">{{--TODO: Href pages when files are made--}}
+										<i class="material-icons">edit</i>
+									</a>
+									<a href="">{{--TODO: Href pages when files are made--}}
+										<i class="material-icons">delete_forever</i>
+									</a>
+								</div>
 							</div>
 						</div>
 					@endforeach
+					<div class="whitespace height-21"></div>
+
+					<div class="dataAddIcon display-inline-block float-right">
+						<a href="{{ route('teacher/poi/create') }}" id="add-item">
+							<i class="material-icons">add</i>
+						</a>
+					</div>
 				</div>
-			</div>
-			<a href="{{ route('teacher/group/create') }}" id="add-item">
-				<i class="material-icons">add</i>
-			</a>
+			</section>
+
+
+
 		</div>
 	</div>
 @endsection
