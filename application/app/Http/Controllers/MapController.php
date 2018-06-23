f7f5403e81181ccf5131dfe1b840331505b9184a
Danny van der Mee <danny.vandermee@edu-kw1c.nl>
2018-06-23 20:04:32 +0200

    Revert stuff

--- application/app/Http/Controllers/MapController.php
+++ application/app/Http/Controllers/MapController.php
@@ -22,6 +22,12 @@ class MapController extends Controller
 
 	public function recievePOI()
 	{
-		foreach($this->PoiController->getPoiForMap())
+		$pois = [
+			['avans', 51.6886659, 5.2869727, 'Description of Avans University POI'],
+			['kw1c', 51.6904646, 5.2867472, 'Description of KW1C POI'],
+			['cs', 51.689968, 5.295078, 'Description of Central Station POI'],
+		];
+
+		return $pois;
 	}
 }
