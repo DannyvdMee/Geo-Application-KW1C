a2053ec3fb12c176929391a7c1534cecfe4dbbf5
Rinaldo Boeje <rinaldo.boeje@edu-kw1c.nl>
2018-06-27 19:32:51 +0200

    Users map aangemaakt, Index aangemaakt

---
+++ application/resources/views/admin/users/index.blade.php
@@ -0,0 +1,61 @@
+{{--TODO: Pois naar users index omschrijven naar users admin page--}}
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col">
+			<!-- Page title -->
+			<div class="row">
+				<div class="col">
+					<h5 class="text-center font-bold">@lang('messages.users')</h5>
+				</div>
+			</div>
+			<!-- End page title -->
+			<!-- POI container -->
+			<div class="row">
+				<div class="col">
+					<div id="poi-container">
+						@foreach($pois as $poi)
+							<div class="row">
+								<div class="col dataItem">
+									<!-- Title -->
+									<p class="display-inline-block">{{ $poi->title }}</p>
+									<div class="float-right">
+										<!-- Visibility(eye) button toggle true -->
+										@if ($poi->visibility == true)
+											<a href="{{ route('teacher/poi/visibility', ['id' => $poi->id]) }}">
+												<i class="material-icons">visibility</i>
+											</a>
+											<!-- Visibility(eye) button toggle false -->
+										@else
+											<a href="{{ route('teacher/poi/visibility', ['id' => $poi->id]) }}">
+												<i class="material-icons">visibility_off</i>
+											</a>
+											<!-- Edit button -->
+										@endif
+										<a href="{{ route('teacher/poi/edit', ['id' => $poi->id]) }}">
+											<i class="material-icons">edit</i>
+										</a>
+										<!-- Delete button -->
+										<a href="{{ route('teacher/poi/delete', ['id' => $poi->id]) }}">
+											<i class="material-icons">delete_forever</i>
+										</a>
+									</div>
+								</div>
+							</div>
+						@endforeach
+						<div class="whitespace height-21"></div>
+
+						<!-- POI add button-->
+						<div class="dataAddIcon display-inline-block float-right">
+							<a href="{{ route('teacher/poi/create') }}" id="add-item">
+								<i class="material-icons">add</i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End POI container -->
+		</div>
+	</div>
+</div>
+
