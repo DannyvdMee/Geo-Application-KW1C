709511864ba33f59e39efe9eab6d639801c30304
Onyi Lam <onyilam@hotmail.com>
2018-06-27 16:53:58 +0200

    Tab

--- application/resources/views/teacher/exercise/create.blade.php
+++ application/resources/views/teacher/exercise/create.blade.php
@@ -14,7 +14,7 @@ Beschrijving:   Create Exercise Pagina
 			<!-- Page title -->
 			<div class="row">
 				<div class="col">
-						<h5 class="text-center font-bold">@lang('messages.exercise-add')</h5>
+					<h5 class="text-center font-bold">@lang('messages.exercise-add')</h5>
 				</div>
 			</div>
 			<!-- End page title -->
