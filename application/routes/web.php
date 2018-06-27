37d07e2305b74fe5315cf7ae7ca3e323afb4a8fb
Danny van der Mee <danny.vandermee@edu-kw1c.nl>
2018-06-27 12:21:44 +0200

    Added the route for login and logout and formatting

--- application/routes/web.php
+++ application/routes/web.php
@@ -1,4 +1,4 @@
-	<?php
+<?php
 
 /*
 |--------------------------------------------------------------------------
@@ -12,9 +12,9 @@
 */
 
 Route::get('/',
-    function () {
-        return view('welcome');
-    });
+	function () {
+		return view('welcome');
+	});
 
 Auth::routes();
 
@@ -23,6 +23,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/map', 'MapController@index')->name('map');
 //Route::get('/map/getPOIS', 'MapController@getPOIS')->name('map/getPOIS');
 Route::get('/map/marker/{id}', 'MapController@retrieveMarkerInfo')->name('map/,arler');
+
+Route::post('/login/request', 'Auth\LoginController@loginRequest')->name('login/request');
+
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+
 
 /*
  * Below routes are for admin users
@@ -34,12 +39,12 @@ Route::get('/map/marker/{id}', 'MapController@retrieveMarkerInfo')->name('map/,a
 
 Route::namespace('Admin')->prefix('admin')->name('admin/')->group(function () {
 
-    Route::get('users', 'UserController@index')->name('users');
+	Route::get('users', 'UserController@index')->name('users');
 
 	Route::get('departments', 'DepartmentController@index')->name('departments');
 	Route::get('departments/create', 'DepartmentController@create')->name('departments/create');
 
-    Route::get('settings', 'SettingsController@index')->name('settings');
+	Route::get('settings', 'SettingsController@index')->name('settings');
 
 });
 
@@ -53,42 +58,42 @@ Route::namespace('Admin')->prefix('admin')->name('admin/')->group(function () {
 
 Route::namespace('Teacher')->prefix('teacher')->name('teacher/')->group(function () {
 
-    //Dashboard
+	//Dashboard
 	Route::get('dashboard', 'DashboardController@index')->name('dashboard');
 
-    //Exercise
-    Route::get('exercise', 'ExerciseController@index')->name('exercise');
+	//Exercise
+	Route::get('exercise', 'ExerciseController@index')->name('exercise');
 	Route::get('exercise/create', 'ExerciseController@create')->name('exercise/create');
 
-    //POI -- Correct en werkend -- kan als template gebruikt worden
-    Route::get('poi', 'PoiController@index')->name('poi');
-    Route::get('poi/create', 'PoiController@create')->name('poi/create');
-    Route::post('poi/create', 'PoiController@store')->name('poi/create');
-    Route::get('poi/edit/{id}', 'PoiController@edit')->name('poi/edit');
-    Route::post('poi/edit/{id}', 'PoiController@update')->name('poi/edit');
-    Route::get('poi/visibility/{id}', 'PoiController@show')->name('poi/visibility');
-    Route::get('poi/delete/{id}', 'PoiController@destroy')->name('poi/delete');
+	//POI -- Correct en werkend -- kan als template gebruikt worden
+	Route::get('poi', 'PoiController@index')->name('poi');
+	Route::get('poi/create', 'PoiController@create')->name('poi/create');
+	Route::post('poi/create', 'PoiController@store')->name('poi/create');
+	Route::get('poi/edit/{id}', 'PoiController@edit')->name('poi/edit');
+	Route::post('poi/edit/{id}', 'PoiController@update')->name('poi/edit');
+	Route::get('poi/visibility/{id}', 'PoiController@show')->name('poi/visibility');
+	Route::get('poi/delete/{id}', 'PoiController@destroy')->name('poi/delete');
 
-    //Routes
-    Route::get('route', 'RouteController@index')->name('route');
-    Route::get('route/create', 'RouteController@create')->name('route/create');
-    Route::post('route/create', 'RouteController@store')->name('route/create');
-    Route::get('route/edit/{id}', 'RouteController@edit')->name('route/edit');
-    Route::post('route/edit/{id}', 'RouteController@update')->name('route/edit');
-    Route::get('route/visibility/{id}', 'RouteController@show')->name('route/visibility');
-    Route::get('route/delete/{id}', 'PoiControRouteControllerller@destroy')->name('route/delete');
+	//Routes
+	Route::get('route', 'RouteController@index')->name('route');
+	Route::get('route/create', 'RouteController@create')->name('route/create');
+	Route::post('route/create', 'RouteController@store')->name('route/create');
+	Route::get('route/edit/{id}', 'RouteController@edit')->name('route/edit');
+	Route::post('route/edit/{id}', 'RouteController@update')->name('route/edit');
+	Route::get('route/visibility/{id}', 'RouteController@show')->name('route/visibility');
+	Route::get('route/delete/{id}', 'PoiControRouteControllerller@destroy')->name('route/delete');
 
-    //Students
-    Route::get('student', 'StudentController@index')->name('student');
-    Route::get('student/create', 'StudentController@create')->name('student/create');
-    Route::post('student/create', 'StudentController@store')->name('student/create');
+	//Students
+	Route::get('student', 'StudentController@index')->name('student');
+	Route::get('student/create', 'StudentController@create')->name('student/create');
+	Route::post('student/create', 'StudentController@store')->name('student/create');
 
-    //Groups
-    Route::get('group', 'GroupController@index')->name('group');
+	//Groups
+	Route::get('group', 'GroupController@index')->name('group');
 	Route::get('group/create', 'GroupController@create')->name('group/create');
 	Route::post('group/create', 'GroupController@store')->name('group/create');
 
-    //Settings
-    Route::get('settings', 'SettingsController@index')->name('settings');
+	//Settings
+	Route::get('settings', 'SettingsController@index')->name('settings');
 
 });
