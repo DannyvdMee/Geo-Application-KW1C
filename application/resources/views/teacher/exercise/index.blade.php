75f7e8ee5c78c9bb6d6767c4207fad46b0f9a73e
Onyi Lam <onyilam@hotmail.com>
2018-06-27 13:45:08 +0200

    Index exercise pagina aangepast

--- application/resources/views/teacher/exercise/index.blade.php
+++ application/resources/views/teacher/exercise/index.blade.php
@@ -1,51 +1,71 @@
 @extends('layouts.teacher')
 
+<!--
+Opdracht:       Multidisciplinair Project v.2
+Auteur:         Onyi Lam, Ibo van Geffen, Rinaldo Boejé, Danny van der Mee
+Aanmaakdatum:   27-06-18
+Beschrijving:   Index Exercise Pagina
+-->
+
 @section('content')
+<div class="container-fluid">
 	<div class="row">
 		<div class="col">
-			<section>
-				<h2 class="text-center">@lang('messages.exercise')</h2>
-
-				<div id="department-container">
+			<!-- Page title -->
+			<div class="row">
+				<div class="col">
+					<h5 class="text-center font-bold">@lang('messages.exercise')</h5>
+				</div>
+			</div>
+			<!-- End page title -->
+			<!-- Exercise container -->
+			<div class="row">
+				<div class="col">
+					<div id="exercise-container">
 						@foreach($exercises as $exercise)
-						<div class="row">
-							<div class="col dataItem">
-								<p class="display-inline-block">{{ $exercise->title }}</p>
-								<div class="float-right">
-									@if ($exercise->state == true)
+							<div class="row">
+								<div class="col dataItem">
+									<!-- Title -->
+									<p class="display-inline-block">{{ $exercise->title }}</p>
+									<div class="float-right">
+										<!-- Visibility(eye) button toggle true -->
+										@if ($exercise->visibility == true)
+											<a href="">{{--TODO: Href pages when files are made--}}
+												<i class="material-icons">visibility</i>
+											</a>
+										<!-- Visibility(eye) button toggle false -->
+										@else
+											<a href="">{{--TODO: Href pages when files are made--}}
+												<i class="material-icons">visibility_off</i>
+											</a>
+										<!-- Edit button -->
+										@endif
 										<a href="">{{--TODO: Href pages when files are made--}}
-											<i class="material-icons">visibility</i>
+											<i class="material-icons">edit</i>
 										</a>
-									@else
+										<!-- Delete button -->
 										<a href="">{{--TODO: Href pages when files are made--}}
-											<i class="material-icons">visibility_off</i>
+											<i class="material-icons">delete_forever</i>
 										</a>
-									@endif
-									<a href="">{{--TODO: Href pages when files are made--}}
-										<i class="material-icons">edit</i>
-									</a>
-									<a href="">{{--TODO: Href pages when files are made--}}
-										<i class="material-icons">delete_forever</i>
-									</a>
+									</div>
 								</div>
 							</div>
+						@endforeach
+						<div class="whitespace height-21"></div>
+
+						<!-- No option to add exercise required. Exercises are linked to a POI
+						<div class="dataAddIcon display-inline-block float-right">
+							<a href="{{ route('teacher/exercise/create') }}" id="add-item">
+								<i class="material-icons">add</i>
+							</a>
 						</div>
-					@endforeach
-					<div class="whitespace height-21"></div>
-
-					{{-- No option to add exercise required. Exercises are linked to a POI
-
-					<div class="dataAddIcon display-inline-block float-right">
-						<a href="{{ route('teacher/exercise/create') }}" id="add-item">
-							<i class="material-icons">add</i>
-						</a>
+						-->
+						
 					</div>
-					--}}
 				</div>
-			</section>
-
-
-
+			</div>
+			<!-- End exercise container -->	
 		</div>
 	</div>
+</div>
 @endsection
