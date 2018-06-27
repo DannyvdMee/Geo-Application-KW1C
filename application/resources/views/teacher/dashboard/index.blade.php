4acc7301a4f9ed044355fb48d73eb8e786846c36
Rinaldo Boeje <rinaldo.boeje@edu-kw1c.nl>
2018-06-27 17:03:01 +0200

    Teacher-dashboard created, Css adjusted (Box size, Font size, Colour)

--- application/resources/views/teacher/dashboard/index.blade.php
+++ application/resources/views/teacher/dashboard/index.blade.php
@@ -3,47 +3,56 @@
 @section('content')
 	<div class="row">
 		<div class="col">
-			<div class="row">
+			<div id="dashboard-container" class="row">
 
 				<!-- De route marker -->
-				<div class="col-6 col-md-3 dashboard-item-background dashboard-item">
-					<div class="float-center display-block">
-						<a href="{{ route('teacher/route') }}">
-							<i class="material-icons display-block">directions</i>
-							<span class="display-block text-center">@lang('messages.routes')</span>
-						</a>
+				<div class="col-6 col-md-3 dashboard-item">
+					<div class="margin-10-0 dashboard-item-background">
+							<div class="float-center display-block">
+							<a href="{{ route('teacher/route') }}">
+								<i class="material-icons display-block dashboard-icon-pos">directions</i>
+								<span class="display-block text-center dashboard-text-pos">@lang('messages.routes')</span>
+							</a>
+						</div>
 					</div>
 				</div>
 
 				<!-- De map marker -->
-				<div class="col-6 col-md-3 dashboard-item-background dashboard-item">
-					<div class="float-center display-block">
-						<a href="{{ route('teacher/route') }}">
-							<i class="material-icons display-block">map</i>
-							<span class="display-block text-center">@lang('messages.routes')</span>
-						</a>
+				<div class="col-6 col-md-3 dashboard-item">
+					<div class="margin-10-0 dashboard-item-background">
+						<div class="float-center display-block">
+							<a href="{{ route('map') }}">
+								<i class="material-icons display-block dashboard-icon-pos">map</i>
+								<span class="display-block text-center dashboard-text-pos">@lang('messages.map')</span>
+							</a>
+						</div>
 					</div>
 				</div>
 
 				<!-- De groep marker -->
-				<div class="col-6 col-md-3 dashboard-item-background dashboard-item">
-					<div class="float-center display-block">
-						<a href="{{ route('teacher/route') }}">
-							<i class="material-icons display-block">group</i>
-							<span class="display-block text-center">@lang('messages.routes')</span>
-						</a>
+				<div class="col-6 col-md-3">
+					<div class="margin-10-0 dashboard-item-background">
+						<div class="float-center display-block dashboard-item">
+							<a href="{{ route('teacher/group') }}">
+								<i class="material-icons display-block dashboard-icon-pos">group</i>
+								<span class="display-block text-center dashboard-text-pos">@lang('messages.groups')</span>
+							</a>
+						</div>
 					</div>
 				</div>
 
 				<!-- De settings marker -->
-				<div class="col-6 col-md-3 dashboard-item-background dashboard-item">
-					<div class="float-center display-block">
-						<a href="{{ route('teacher/route') }}">
-							<i class="material-icons display-block">settings</i>
-							<span class="display-block text-center">@lang('messages.routes')</span>
-						</a>
+				<div class="col-6 col-md-3 dashboard-item">
+					<div class="margin-10-0 dashboard-item-background">
+						<div class="float-center display-block">
+							<a href="{{ route('teacher/settings') }}">
+								<i class="material-icons display-block dashboard-icon-pos">settings</i>
+								<span class="display-block text-center dashboard-text-pos">@lang('messages.settings')</span>
+							</a>
+						</div>
 					</div>
 				</div>
+
 
 			</div>
 		</div>
