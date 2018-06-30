ba13c1d60b69b918a79c3b7ab43fb744d5027459
Onyi Lam <onyilam@hotmail.com>
2018-06-30 15:55:03 +0200

    Backend controllers, migrations en seeder aangepast

--- application/app/Http/Controllers/Admin/UserController.php
+++ application/app/Http/Controllers/Admin/UserController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 use App\User;
+use App\Department;
 
 class UserController extends Controller
 {
@@ -16,9 +18,11 @@ class UserController extends Controller
 	 */
 	public function index()
 	{
-		$users = User::all();
+		$users = User::where('active', '=', 1)->orderBy('lastname', 'asc')->get();
 
-		return view('admin/users/index');
+		$departments = Department::where('active', '=', 1)->get();
+
+		return view('admin/user/index',  ['users' => $users, 'departments' => $departments]);
 	}
 
 	/**
@@ -28,7 +32,9 @@ class UserController extends Controller
 	 */
 	public function create()
 	{
-		return view('admin/users/create');
+		$departments = Department::where('active', '=', 1)->get();
+
+		return view('admin/user/create', ['departments' => $departments]);
 	}
 
 	/**
@@ -43,6 +49,75 @@ class UserController extends Controller
 		$user = new User;
 
 		$user->email = $request->email;
+		$user->username = $request->username;
+		$user->password = Hash::make($request->password);
+
+		$user->firstname = $request->firstname;
+		$user->lastname = $request->lastname;
+
+		$user->department = $request->department;
+		$user->active = $request->active;
+
+		$user->save();
+
+		return redirect('admin/user');
+	}
+	
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+		$user = User::find($id);
+
+		if ($user->visibility == 1) {
+			$visibility = 0;
+		} else if ($user->visibility == 0) {
+			$visibility = 1;
+		}
+
+		$user->visibility = $visibility;
+
+        $user->save();
+        
+        return redirect('admin/user');
+	}
+	
+	/**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+		$user = User::find($id);
+		
+		$departments = Department::where('active', '=', 1)->get();
+
+        return view('admin/user/edit', ['user' => $user, 'departments' => $departments]);
+	}
+	
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int                      $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+		$user = User::find($id);
+
+		$user->email = $request->email;
+		$user->username = $request->username;
+
 		$user->firstname = $request->firstname;
 		$user->lastname = $request->lastname;
 		$user->department = $request->department;
@@ -51,7 +126,25 @@ class UserController extends Controller
 
 		$user->save();
 
-		return redirect('admin/users');
-	}
+		return redirect('admin/user');
+    }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+		$user = User::find($id);
+
+		$user->active = 0;
+
+		$user->save();
+
+        return redirect('admin/user');
+	}
+	
 }
