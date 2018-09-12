27c2b03af90fc412d94d03797d161465b9fcface
Danny van der Mee <danny.vandermee@edu-kw1c.nl>
2018-09-12 09:10:07 +0200

    Formatted the Repo Interfaces, because it looked so ugly

--- application/app/Repositories/Poi/PoiInterface.php
+++ application/app/Repositories/Poi/PoiInterface.php
@@ -5,11 +5,17 @@ namespace App\Repositories\Poi;
 interface PoiInterface
 {
 	public function getOne($id);
+
 	public function getAllActive();
+
 	public function getLatestPoi();
+
 	public function store($data);
+
 	public function update($data, $id);
+
 	public function softDelete($data);
+
 	public function forceDelete($data);
 
 }
--- application/app/Repositories/StudentGroup/StudentGroupInterface.php
+++ application/app/Repositories/StudentGroup/StudentGroupInterface.php
@@ -5,10 +5,15 @@ namespace App\Repositories\StudentGroup;
 interface StudentGroupInterface
 {
 	public function getOne($id);
+
 	public function getAllActive();
+
 	public function store($data);
+
 	public function update($data, $id);
+
 	public function softDelete($data);
+
 	public function forceDelete($data);
 
 }
