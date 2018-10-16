a4894705152701a2c0b01f38af3a0896289d5c5e
Ibo van Geffen <ibovangeffen@gmail.com>
2018-10-16 11:44:06 +0200

    Removed some obsolete code

--- application/resources/views/layouts/admin.blade.php
+++ application/resources/views/layouts/admin.blade.php
@@ -47,49 +47,6 @@
 					<span class="display-block">@lang('messages.logout')</span>
 				</a>
 			</div>
-
-			<!-- WTF is dit? -->
-			<!-- <div class="container">
-				<a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name') }}</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse"
-						data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-						aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					Left Side Of Navbar
-					<ul class="navbar-nav mr-auto">
-
-					</ul>
-
-					Right Side Of Navbar
-					<ul class="navbar-nav ml-auto">
-						Authentication Links
-						@guest
-							<li><a class="nav-link" href="{{ route('login') }}">@lang('messages.login')</a></li>
-							<li><a class="nav-link" href="{{ route('register') }}">@lang('messages.register')</a></li>
-						@else
-							<li class="nav-item dropdown">
-								<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-								   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-									{{ Auth::user()->name }} <span class="caret"></span>
-								</a>
-
-								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-									<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-										@lang('messages.logout')
-									</a>
-
-									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-										@csrf
-									</form>
-								</div>
-							</li>
-						@endguest
-					</ul>
-				</div>
-			</div> -->
 			
 		</nav>
 	</header>
@@ -108,26 +65,6 @@
 			</div>
 		</div>
 	</footer>
-
-	<!-- <footer>
-		<div class="float-left display-inline-block">
-			<a href="{{ route('map') }}">
-				<i class="material-icons display-block">domain</i>
-				<span class="display-block text-center">@lang('messages.departments')</span>
-			</a>
-		</div><div class="float-left display-inline-block">
-			<a href="{{ route('admin/user') }}">
-				<i class="material-icons display-block">person</i>
-				<span class="display-block text-center">@lang('messages.users')</span>
-			</a>
-		</div>
-		<div class="float-right display-inline-block">
-			<a href="{{ route('admin/settings') }}">
-				<i class="material-icons display-block">settings</i>
-				<span class="display-block text-center">@lang('messages.settings')</span>
-			</a>
-		</div>
-	</footer> -->
 </div>
 @yield('js-eventlisteners')
 </body>
